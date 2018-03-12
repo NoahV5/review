14c198bfa8520befe5c563300101c15070658cce
NoahV5 <15989040320@139.com>
2018-03-12 09:35:23 +0800

    first commit

---
+++ service-hi/src/main/java/com/victor/service/HiService.java
@@ -0,0 +1,15 @@
+package com.victor.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Administrator on 2018/2/28 0028.
+ */
+@Service
+public class HiService {
+
+    public String sayHi(){
+
+        return "一切都会好起来的！";
+    }
+}
