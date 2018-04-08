41d6ebfc296263f2f69717da06ff7a5779cd3539
Victor <15989040320@139.com>
2018-04-08 10:24:00 +0800

    rop模块

---
+++ server-rop/src/main/java/com/victor/ServerRopApplication.java
@@ -0,0 +1,12 @@
+package com.victor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerRopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerRopApplication.class, args);
+	}
+}
